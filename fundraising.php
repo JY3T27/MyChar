6d10845b98d6a3a93ab2eb5a86272c0d877f5885
JY3T27 <jjlee2703@gmail.com>
2024-12-30 22:30:16 +0800

    30/12

--- fundraising.php
+++ fundraising.php
@@ -11,50 +11,31 @@ include 'config.php';
     ?>
     <script>
         function enableEdit() {
-            const element = document.getElementById("profile_name");
-            var userRole = "<?php echo $_SESSION["role"] ?>";
+            const element = document.getElementById("fundraising_title");
             if (element.disabled) {
-                document.getElementById("profile_name").disabled = false;
+                document.getElementById("fundraising_title").disabled = false;
+                document.getElementById("fundraising_desc").disabled = false;
                 document.getElementById("updateBtn").style.display = "block";
                 document.getElementById("resetBtn").style.display = "block";
-                if (userRole == "charity") {
-                    document.getElementById("profile_phone").disabled = false;
-                    document.getElementById("profile_contactEmail").disabled = false;
-                    document.getElementById("profile_address").disabled = false;
-                    document.getElementById("profile_state").disabled = false;
-                    document.getElementById("profile_URL").disabled = false;
-                    document.getElementById("profile_code").disabled = false;
-                    document.getElementById("profile_desc").disabled = false;
-                } else if (userRole == "admin") {
-                    document.getElementById("website_address1").disabled = false;
-                    document.getElementById("website_address2").disabled = false;
-                    document.getElementById("website_phone").disabled = false;
-                    document.getElementById("website_email").disabled = false;
-                }
             } else {
-                document.getElementById("profile_name").disabled = true;
+                document.getElementById("fundraising_title").disabled = true;
+                document.getElementById("fundraising_desc").disabled = true;
                 document.getElementById("updateBtn").style.display = "none";
                 document.getElementById("resetBtn").style.display = "none";
-                if (userRole == "charity") {
-                    document.getElementById("profile_phone").disabled = true;
-                    document.getElementById("profile_contactEmail").disabled = true;
-                    document.getElementById("profile_address").disabled = true;
-                    document.getElementById("profile_state").disabled = true;
-                    document.getElementById("profile_URL").disabled = true;
-                    document.getElementById("profile_code").disabled = true;
-                    document.getElementById("profile_desc").disabled = true;
-                } else if (userRole == "admin") {
-                    document.getElementById("website_address1").disabled = true;
-                    document.getElementById("website_address2").disabled = true;
-                    document.getElementById("website_phone").disabled = true;
-                    document.getElementById("website_email").disabled = true;
-                }
             }
         }
 
         function confirmationDlt() {
-            if (confirm("Are you sure you want to delete your account?")) {
-                return true;
+            if (confirm("Are you sure you want to delete this fundraising?")) {
+                const confirmationMessage = "Please CONFIRM that you deleting this fundraising? Type DELETE to confirm:";
+                const userInput = prompt(confirmationMessage, "");
+                if (userInput === "DELETE") {
+                    return true;
+                } else {
+                    alert("Deletion cancelled.");
+                    window.location.href = "fundraising_charity.php";
+                    return false;
+                }
             } else {
                 return false;
             }
@@ -99,7 +80,7 @@ include 'config.php';
                                 <div class="profile-btn col">
                                     <button type="button" id="togglePassword" onclick="enableEdit()" class="btn shadow-none bg-transparent border-0">
                                         <i class="fa fa-pencil-square-o fs-2" aria-hidden="true"></i></button>
-                                    <button type="submit" id="togglePassword" onclick="return confirm('Confirm delete account?')" name="action" value="Delete" class="btn shadow-none bg-transparent border-0">
+                                    <button type="submit" id="togglePassword" onclick="return confirmationDlt()" name="action" value="Delete" class="btn shadow-none bg-transparent border-0">
                                         <i class="fa fa-trash fs-2" aria-hidden="true"></i></button>
                                 </div>
                             </div>
@@ -127,12 +108,13 @@ include 'config.php';
                                             <label for="fundraising_title" class="col-md-4 col-form-label text-md-end">Title</label>
                                             <div class="col-md-6">
                                                 <input id="fundraising_title" type="text" class="form-control" name="fundraising_title" value="<?= $title ?>" disabled>
+                                                <input id="fundraising_id" type="text" class="form-control" name="fundraising_id" value="<?= $fundID ?>" hidden>
                                             </div>
                                         </div>
                                         <div class="row mb-3">
                                             <label for="fundraising_date" class="col-md-4 col-form-label text-md-end">Date created</label>
                                             <div class="col-md-6">
-                                                <input id="fundraising_date" type="date" class="form-control" name="fundraising_date" value="<?= $date ?>" required autocomplete="email" disabled>
+                                                <input id="fundraising_date" type="text" class="form-control" name="fundraising_date" value="<?= $date ?>" required autocomplete="email" disabled>
                                             </div>
                                         </div>
                                         <div class="row mb-3">
@@ -175,58 +157,31 @@ include 'config.php';
         include 'layout/footer.php';
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $action = $_POST["action"];
-            $userID = $_SESSION["UID"];
-            $role = $_SESSION["role"];
+            $fundingID = $_POST['fundraising_id'];
             if ($action == "Update") {
-                if ($role == 'donor') {
-                    $edited_name = $_POST['profile_name'];
-                    $sql = "UPDATE donor SET donor_name = '$edited_name' WHERE donor.user_id = '$userID'";
-                    if (mysqli_query($conn, $sql)) {
-                        echo '<script>alert("Edit Successfully."); window.location.href = "profile.php";</script>';
-                    } else {
-                        echo '<script>alert("Error");</script>';
-                    }
-                } elseif ($role == 'charity') {
-                    $edited_name = $_POST['profile_name'];
-                    $edited_phone = $_POST['profile_phone'];
-                    $edited_email = $_POST['profile_contactEmail'];
-                    $edited_address = $_POST['profile_address'];
-                    $edited_state = $_POST['profile_state'];
-                    $edited_URL = $_POST['profile_URL'];
-                    $edited_code = $_POST['profile_code'];
-                    $edited_desc = $_POST['profile_desc'];
-                    $sql = "UPDATE charity SET charity_name = '$edited_name', charity_phoneNo = '$edited_phone', charity_contactEmail = '$edited_email', 
-                            charity_address = '$edited_address', charity_state = '$edited_state', charity_websiteURL = '$edited_URL', 
-                            charity_code = '$edited_code', charity_desc = '$edited_desc'
-                            WHERE charity.user_id = '$userID'";
-                    if (mysqli_query($conn, $sql)) {
-                        echo '<script>alert("Edit Successfully."); window.location.href = "profile.php";</script>';
-                    } else {
-                        echo '<script>alert("Error");</script>';
-                    }
-                } elseif ($role == 'admin') {
-                    $edited_name = $_POST['profile_name'];
-                    $edited_address1 = $_POST['website_address1'];
-                    $edited_address2 = $_POST['website_address2'];
-                    $edited_phone = $_POST['website_phone'];
-                    $edited_email = $_POST['website_email'];
-                    $sql = "UPDATE admin SET admin_name = '$edited_name', website_phoneNo = '$edited_phone', website_email = '$edited_email', 
-                            website_address1 = '$edited_address1', website_address2 = '$edited_address2'   
-                            WHERE admin.user_id = '$userID'";
-                    if (mysqli_query($conn, $sql)) {
-                        echo '<script>alert("Edit Successfully."); window.location.href = "profile.php";</script>';
-                    } else {
-                        echo '<script>alert("Error");</script>';
-                    }
+                $edited_title = $_POST['fundraising_title'];
+                $edited_desc = $_POST['fundraising_desc'];
+                $sql = "UPDATE fundraising SET fundraising_title = '$edited_title', fundraising_desc = '$edited_desc'
+                        WHERE fundraising_id = '$fundingID'";
+                if (mysqli_query($conn, $sql)) {
+                    echo '<script>alert("Edit Successfully."); window.location.href = "fundraising_charity.php";</script>';
                 } else {
                     echo '<script>alert("Error");</script>';
                 }
             } elseif ($action == "Delete") {
-                $sql = "DELETE FROM users WHERE users.user_id = '$userID'";
+                $sql = "DELETE FROM fundraising_update WHERE fundraising_id = '$fundingID'
+                        DELETE FROM donation WHERE fundraising_id = '$fundingID'
+                        DELETE FROM fundraising WHERE fundraising_id = '$fundingID'";
                 if (mysqli_query($conn, $sql)) {
-                    echo '<script>alert("Delete Successfully."); window.location.href = "logout.php";</script>';
+                    echo '<script>alert("Delete Successfully."); window.location.href = "fundraising_charity.php";</script>';
                 } else {
-                    echo '<script>alert("Error");</script>';
+                    // Check for foreign key violation error (error code 1451)
+                    if ($conn->errno == 1451) {
+                        echo '<script>alert("The row can\'t be deleted due to it containing child rows.");</script>';
+                    } else {
+                        // General error handling
+                        echo '<script>alert("Error: ' . $conn->error . '");</script>';
+                    }
                 }
             } elseif ($action == "ChangePP") {
                 echo '<script>window.location.href = "profilePic.php";</script>';
